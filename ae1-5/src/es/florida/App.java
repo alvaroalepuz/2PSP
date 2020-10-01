cd5d60885ffb1c9e9ce165ce03ac8cc942ae3822
Alvaro Alepuz <alvaroalepuz99@gmail.com>
2020-10-01 20:49:53 +0200

    Ejercicio5

---
+++ ae1-5/src/es/florida/App.java
@@ -0,0 +1,11 @@
+package es.florida;
+
+import java.util.ArrayList;
+
+public class App {
+
+    public static void mayor(ArrayList<String> lista){
+
+        System.out.println(lista.get(lista.size()-1));
+    }
+}
---
+++ ae1-5/src/es/florida/Main.java
@@ -0,0 +1,15 @@
+package es.florida;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> lista = new ArrayList<String>();
+        lista.add("Pepe");
+        lista.add("Manolo");
+        lista.add("Jose");
+        App.mayor(lista);
+    }
+}
